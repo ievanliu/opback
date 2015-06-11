37b8f7c9ca07353f36bf72462a25fa41475ffc0c
promisejohn <promise.john@gmail.com>
2015-06-11 23:09:24 +0800

    Done project structure.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='helloworld',
+      version='0.1.0',
+      description='hello world for python dev',
+      author='promisejohn',
+      author_email='promise.john@gmail.com',
+      url='http://promisejohn.github.io',
+      packages=['hello'],
+      # py_modules=['app']
+      # install_requires = [],
+      # dependency_links = []
+      )
---
+++ src/hello/app.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+#
+# Author: promisejohn
+# Email: promise.john@gmail.com
+#
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a+b)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
