57698308ecd54688ff989841a39d884e67348258
seethedoor <dai.sheng@139.com>
2015-11-26 10:50:09 +0800

    load the instance/config.py only when it is exis

--- promise/__init__.py
+++ promise/__init__.py
@@ -18,8 +18,10 @@ app = Flask(__name__, instance_relative_config=True)
 # Config File:
 # use the instance/config.py to cover the default config object.
 # u can put some instance settings into the instance/config.py
+
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+if os.path.isfile('/instance/config.py'): #如果不存在就返回False
+	app.config.from_pyfile('config.py')
 
 # Init The Api Obj
 from flask.ext.restful import Api
