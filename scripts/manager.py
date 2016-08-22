228fce0967269a6af1572da9849bf86cd69f5e74
seethedoor <dai.sheng@139.com>
2016-08-22 11:48:21 +0800

    use new API to init data

--- scripts/manager.py
+++ scripts/manager.py
@@ -31,64 +31,86 @@ def importdata():
     "Import data into database tables"
 
     # init privileges
-    privilegeNameList = [
-        'userAdmin', 'inventoryAdmin', 'shellExec', 'scriptExec', 'walkerInfo']
-    privilegeList = []
-    for item in privilegeNameList:
-        newPrivilege = Privilege(item)
-        db.session.add(newPrivilege)
-        privilegeList.append(newPrivilege)
+#    privilege_name_list = [
+#        'userAdmin', 'inventoryAdmin', 'shellExec', 'scriptExec', 'walkerInfo']
+#    privilege_list = []
+#    for item in privilege_name_list:
+#        newPrivilege = Privilege(item)
+#        db.session.add(newPrivilege)
+#        privilegeList.append(newPrivilege)
+    user_admin_privilege = Privilege(privilege_name='userAdmin',
+        description='user/role/privlilege administration.')
+    inventory_admin_privilege = Privilege(privilege_name='InventoryAdmin',
+        description='cmdb/inventory administration')
+    shell_exec_privilege = Privilege(privilege_name='shellExec',
+        description='execution of shell module of walker.')
+    script_exec_privilege = Privilege(privilege_name='scriptExec',
+        description='execution of script module of walker.')
+    walker_info_privilege = Privilege(privilege_name='walkerInfo',
+        description='get the details infomation of walkers.')
+    user_admin_privilege.save()
+    inventory_admin_privilege.save()
+    shell_exec_privilege.save()
+    script_exec_privilege.save()
+    walker_info_privilege.save()
+
+
 
     # init roles
-    roleRoot = Role('root')
-    roleOperator = Role('operator')
-    roleInventoryAdmin = Role('InventoryAdmin')
-    roleUserAdmin = Role('userAdmin')
+    role_root = Role(role_name='root', description='超级用户')
+    role_operator = Role(role_name='operator', description='运维操作员')
+    role_inventory_admin = Role(
+        role_name='InventoryAdmin', description='资源管理员')
+    role_user_admin = Role(role_name='userAdmin', description='用户管理员')
 
-    inventoryAdminPrivilege = Privilege.getFromPrivilegeName('InventoryAdmin')
-    userAdminPrivilege = Privilege.getFromPrivilegeName('userAdmin')
-    shellExecPrivilege = Privilege.getFromPrivilegeName('shellExec')
-    scriptExecPrivilege = Privilege.getFromPrivilegeName('scriptExec')
-    walkerInfoPrivilege = Privilege.getFromPrivilegeName('walkerInfo')
+    role_root.update(
+        privileges=[
+            inventory_admin_privilege, user_admin_privilege,
+            shell_exec_privilege, script_exec_privilege,
+            walker_info_privilege])
+    role_user_admin.update(privileges=[user_admin_privilege])
+    role_inventory_admin.update(privileges=[inventory_admin_privilege])
+    role_operator.update(
+        privileges=[
+            shell_exec_privilege, script_exec_privilege,
+            walker_info_privilege])
 
-    roleRoot.addPrivilege(privilegeList=privilegeList)
-    roleUserAdmin.addPrivilege(privilege=userAdminPrivilege)
-    roleInventoryAdmin.addPrivilege(privilege=inventoryAdminPrivilege)
-    roleOperator.addPrivilege(
-        privilegeList=[
-            shellExecPrivilege, scriptExecPrivilege, walkerInfoPrivilege])
-
-    db.session.add(roleRoot)
-    db.session.add(roleOperator)
-    db.session.add(roleInventoryAdmin)
-    db.session.commit()  # commit the roles before user init
+#    db.session.add(roleRoot)
+#    db.session.add(roleOperator)
+#    db.session.add(roleInventoryAdmin)
+#    db.session.commit()  # commit the roles before user init
+    role_root.save()
+    role_user_admin.save()
+    role_inventory_admin.save()
+    role_operator.save()
 
     # init users
     user1 = User(
-        user_name='tom',
+        username='tom',
         hashed_password=userUtils.hash_pass("tompass"),
-        role_list=[roleOperator])
+        role_list=[role_operator])
     user2 = User(
-        user_name='jerry',
+        username='jerry',
         hashed_password=userUtils.hash_pass("jerrypass"),
-        role_list=[roleInventoryAdmin])
+        role_list=[role_inventory_admin])
     user3 = User(
-        user_name='mike',
+        username='mike',
         hashed_password=userUtils.hash_pass("mikepass"),
-        role_list=[roleUserAdmin])
-    #user2.addRole(role=roleInventoryAdmin)
-    rootUser = User(
-        user_name=app.config['DEFAULT_ROOT_USER_NAME'],
-        hashed_password=userUtils.hash_pass(app.config['DEFAULT_ROOT_PASSWORD']),
-        role_list=[roleRoot])
+        role_list=[role_user_admin])
+    # user2.addRole(role=roleInventoryAdmin)
+    root_user = User(
+        username=app.config['DEFAULT_ROOT_USERNAME'],
+        hashed_password=userUtils.hash_pass(
+            app.config['DEFAULT_ROOT_PASSWORD']),
+        role_list=[role_root])
     visitor = User(
-        user_name='visitor',
+        username='visitor',
         hashed_password=userUtils.hash_pass('visitor'))
-    
+
     user1.save()
     user2.save()
     user3.save()
-    rootUser.save()
+    root_user.save()
     visitor.save()
     print 'Data imported'
 
