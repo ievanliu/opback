36ee125b227627dc18fe06a8bab089e902535ed1
seethedoor <dai.sheng@139.com>
2016-09-05 16:33:36 +0800

    tornado flake8

--- tornado_run.py
+++ tornado_run.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 #
-# Author: promisejohn
-# Email: promise.john@gmail.com
+# Author: Shawn.Tai
+# Email: shawntai.ds@gmail.com
 #
 
 from promise import app
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+
 app.config.update(DEBUG=True)
 
 http_server = HTTPServer(WSGIContainer(app))
