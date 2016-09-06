27d60e5a8e2896df01e48b5a1b28830178cc0b45
seethedoor <dai.sheng@139.com>
2016-09-06 19:03:25 +0800

    add port parms

--- tornado_run.py
+++ tornado_run.py
@@ -8,9 +8,18 @@ from promise import app
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+import tornado.options
 
 app.config.update(DEBUG=True)
 
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
-IOLoop.instance().start()
+from tornado.options import define, options
+define("port", default=5000, help="run on the given port", type=int)
+
+# http_server = HTTPServer(WSGIContainer(app))
+# http_server.listen(options.port)
+# IOLoop.instance().start()
+if __name__=="__main__":
+    tornado.options.parse_command_line()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(options.port)
+    IOLoop.instance().start()
