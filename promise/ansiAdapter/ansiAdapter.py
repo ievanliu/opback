f6b4502b341704b40d22ccb76bbf5fcbe48d97b1
leannmak <leannmak@139.com>
2016-09-21 01:58:11 +0800

    fix flake8

--- promise/ansiAdapter/ansiAdapter.py
+++ promise/ansiAdapter/ansiAdapter.py
@@ -249,9 +249,9 @@ class ScriptExecAdapter(object):
             gather_facts='no',
             tasks=[
                 dict(
-                    script=self.script_file.name + ' ' + \
+                    script=self.script_file.name + ' ' +
                     params.encode('utf-8'),
-                     register='script_out', no_log=True)])
+                    register='script_out', no_log=True)])
 #            tasks=[
 #                dict(script=self.script_file.name + ' ' + str(params),
 #                     register='script_out', no_log=True),
