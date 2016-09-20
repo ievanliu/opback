88b653277a76a546f9bed5e6b7f96bd9ac92c679
leannmak <leannmak@139.com>
2016-09-20 22:31:14 +0800

    add forward-related init data

--- scripts/manager.py
+++ scripts/manager.py
@@ -192,27 +192,74 @@ def eater_importdata():
     "import data to eater"
     # init it model
     model = ITModel()
-    model.insert(name='bclinux7', vender='bclinux7')
-    model.insert(name='adx03100', vender='brocade')
-    model.insert(name='usg1000', vender='Qimingxingchen')
-    model.insert(name='mx960', vender='Juniper')
-    model.insert(name='asa', vender='Cisco')
-    model.insert(name='c4510', vender='Cisco')
-    model.insert(name='c6509', vender='Cisco')
-    model.insert(name='5548', vender='Cisco')
+    model.insert(name='bclinux7', vender='bclinux7', id='m-0000')
+    model.insert(name='sr7750', vender='bel', id='m-0001')
+    model.insert(name='asa', vender='Cisco', id='m-0002')
+    model.insert(name='asr1006', vender='Cisco', id='m-0003')
+    model.insert(name='c2960', vender='Cisco', id='m-0004')
+    model.insert(name='c4510', vender='Cisco', id='m-0005')
+    model.insert(name='c6509', vender='Cisco', id='m-0006')
+    model.insert(name='n5548', vender='Cisco', id='m-0007')
+    model.insert(name='n5596', vender='Cisco', id='m-0008')
+    model.insert(name='n7010', vender='Cisco', id='m-0009')
+    model.insert(name='n7018', vender='Cisco', id='m-0010')
+    model.insert(name='n7710', vender='cisco', id='m-0011')
+    model.insert(name='n7718', vender='cisco', id='m-0012')
+    model.insert(name='fw1000', vender='depp', id='m-0013')
+    model.insert(name='m6000', vender='depp', id='m-0014')
+    model.insert(name='f510000', vender='f5', id='m-0015')
+    model.insert(name='s5800', vender='fenghuo', id='m-0016')
+    model.insert(name='fg3040', vender='Fortinet', id='m-0017')
+    model.insert(name='fg3950', vender='Fortinet', id='m-0018')
+    model.insert(name='e8000e', vender='huawei', id='m-0019')
+    model.insert(name='s9303', vender='Huawei', id='m-0020')
+    model.insert(name='s9312', vender='huawei', id='m-0021')
+    model.insert(name='mx960', vender='Juniper', id='m-0022')
+    model.insert(name='s3300', vender='Maipu', id='m-0023')
+    model.insert(name='usg1000', vender='Qimingxingchen', id='m-0024')
+    model.insert(name='r3048g', vender='raisecom', id='m-0025')
+    model.insert(name='af6020', vender='sinfor', id='m-0026')
+    model.insert(name='m6000', vender='zte', id='m-0027')
     # init connection
     connect = Connection()
-    connect.insert(method='ssh', port=22)
-    connect.insert(method='telnet', port=23)
+    connect.insert(method='ssh', port=22, id='conn-0001')
+    connect.insert(method='telnet', port=23, id='conn-0002')
     # init os user
     osuser = OSUser()
-    connect = Connection.query.filter_by(method='ssh', port=22).all()
+    connect1 = Connection.query.filter_by(id='conn-0001').first()
+    connect2 = Connection.query.filter_by(id='conn-0002').first()
     osuser.insert(
-        name='python_script', con_pass='4W@1lHere', act_pass='EREh1L@MR0F',
-        connect=connect)
+        name='python_script',
+        con_pass='50526d5765504b52ad9234f8c6723e5352d966203cd701401a006955abf8'
+                 '92e6fa5d88e4a740fc95c4b33dad17518c825dafb65c9ad891ed90d1634f'
+                 '532b55b1249f821a2e8480b22aa25350673cdd75b15d9cf3bf156d79168e'
+                 '552ffa3a362f9bf6af5b1af1a79c22cd3d8f9b8e42806ea95227613b5f5c'
+                 '7b298375a4563199',
+        connect=[connect1, connect2])
     osuser.insert(
-        name='root', con_pass='hey!@mR0ot', act_pass='to0Rm@!yeh',
-        connect=connect)
+        name='python_script',
+        con_pass='4a6f9a31a1033319f3bf3852c8eef2ce62c607c5d40b50670234a2ea72ca'
+                 'af77668f34a4f32c422be4f107071c370cde9c9b872134414674fe8f457c'
+                 'a2489fd55b5e2b637d0536ba5dc42815b55ed4a83eb1603b0af7edc8f5cc'
+                 '4549f58344e724cb602dadbc39d1183cd50a16aa56da0ac4aaba1638e2d1'
+                 '80c053212e33a680',
+        connect=[connect1])
+    osuser.insert(
+        name='admin',
+        con_pass='50526d5765504b52ad9234f8c6723e5352d966203cd701401a006955abf8'
+                 '92e6fa5d88e4a740fc95c4b33dad17518c825dafb65c9ad891ed90d1634f'
+                 '532b55b1249f821a2e8480b22aa25350673cdd75b15d9cf3bf156d79168e'
+                 '552ffa3a362f9bf6af5b1af1a79c22cd3d8f9b8e42806ea95227613b5f5c'
+                 '7b298375a4563199',
+        connect=[connect1])
+    osuser.insert(
+        name='wangluozu',
+        con_pass='5269742d141b86c0856012d2ba344c736a54ec85df77f93a0d433e9827fb'
+                 '259f0355dec6ab5a09698b2cdfcdf84acf0b8cbcef4ea9b8da6c9699f2ed'
+                 '748d90af987000878b1c1581301af2c1e9d638432f46c3ac5f8e65dd640a'
+                 '63d32699cf16d8f4b20932f5cf2b68d39a1c33276c88f2d579137e8f593a'
+                 '28f194eaa7f1d407',
+        connect=[connect1])
 
 
 @manager.command
